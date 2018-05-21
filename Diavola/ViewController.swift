743f17fb561cff9db73a7d7297d83d2047985c34
LucasAssisRo <lucas.assis.ro@gmail.com>
2018-05-21 15:35:18 +0200

    first commit

---
+++ Diavola/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diavola
+//
+//  Created by Lucas Assis Rodrigues on 21/05/2018.
+//  Copyright © 2018 Lucas Assis Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-DiavolaUITests/Pods-DiavolaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiavolaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DiavolaUITests
+@end
